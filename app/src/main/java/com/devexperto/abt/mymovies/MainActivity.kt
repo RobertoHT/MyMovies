ab70a17c43b2ffe1f8e9d4c8e6a1006847e9282f
Roberto Hernández Tamayo <roberto.htamayo@gmail.com>
2022-02-18 18:36:38 -0600

    Agregando el adapter y funcionamiento del mismo para un listado estatico

--- app/src/main/java/com/devexperto/abt/mymovies/MainActivity.kt
+++ app/src/main/java/com/devexperto/abt/mymovies/MainActivity.kt
@@ -2,10 +2,22 @@ package com.devexperto.abt.mymovies
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.devexperto.abt.mymovies.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.recycler.adapter = MoviesAdapter(listOf(
+            Movie("Title 1", "https://loremflickr.com/320/240?lock=1"),
+            Movie("Title 2", "https://loremflickr.com/320/240?lock=2"),
+            Movie("Title 3", "https://loremflickr.com/320/240?lock=3"),
+            Movie("Title 4", "https://loremflickr.com/320/240?lock=4")
+        )) {
+            movie -> Toast.makeText(this@MainActivity, movie.title, Toast.LENGTH_SHORT).show()
+        }
     }
 }
---
+++ app/src/main/java/com/devexperto/abt/mymovies/Movie.kt
@@ -0,0 +1,3 @@
+package com.devexperto.abt.mymovies
+
+data class Movie(val title: String, val cover: String)
